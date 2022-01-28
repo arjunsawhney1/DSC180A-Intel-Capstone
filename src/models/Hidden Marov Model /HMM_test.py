7599844e9b8baaa837856879e2d21b90c7cc73e6
Srikar Prayaga <sprayaga@ucsd.edu>
2022-01-27 23:43:26 -0800

    hmm files

--- src/models/Hidden Marov Model /HMM_test.py
+++ src/models/Hidden Marov Model /HMM_test.py
@@ -1,23 +1,27 @@
 import pandas as pd
-class HMM:
-    def __init__(self,data):
-        self.data=data
+import math
 
-    def get_next_window(win, df):
+class HMM:
+    def __init__(self):
+        self.temp = []
+        
+    def get_next_window(self, win, df):
         windows_after_win = []
         for i in range(len(df) - 1):
             if df['window'].iloc[i] == win:
                 windows_after_win.append(df['window'].iloc[i+1])
         return windows_after_win
-    def get_cond_probs(win,df):
-        temp_windows = pd.Series(get_next_window(win, df))
+
+    def get_cond_probs(self, win,df):
+        temp_windows = pd.Series(self.get_next_window(win, df))
         return temp_windows.value_counts() / temp_windows.value_counts().sum()
-    def transition_matrix(data):
+
+    def transition_matrix(self,data):
         window_order = list(data.window.unique())
         trans_matrix = []
         for curr_win in window_order:
             curr_row  = [0] * len(window_order)
-            probs = get_cond_probs(curr_win, data)
+            probs = self.get_cond_probs(curr_win, data)
             probs_dict = dict(probs)
             for win in probs.index:
                 prob = probs_dict[win]
@@ -25,28 +29,30 @@ class HMM:
                 curr_row[i] = prob
             trans_matrix.append(curr_row)
         return trans_matrix
-    def get_immersive_prob(win, data):
+
+    def get_immersive_prob(self, win, data):
         win_data = data[data.window == win]
         total_is_immersive = win_data.is_immersive.value_counts().sum() #this doesn't count values of NaN
         num_immersive = len(win_data[win_data.is_immersive == 1])
         return num_immersive / total_is_immersive 
-    def emission_matrix(data):
+        
+    def emission_matrix(self,data):
         window_order = list(data.window.unique())
         emis_matrix = []
         for win in window_order:
-            emis_matrix.append(get_immersive_prob(win, data))
+            emis_matrix.append(self.get_immersive_prob(win, data))
         emis_matrix = [0.0 if math.isnan(x) else x for x in emis_matrix]
         return [emis_matrix]  
-        """
+
+    """
     Cleans csv and outputs either a windows df OR windows and is_immersive df
     @params: data str csv path to just windows data 
             data_imm str csv path to immersive data
     @return: df
     """
-    def clean_data(window_path, imm_path = 1):
-        data = _
+    def clean_data(self,window_path, imm_path = 1): 
         #if immersive data is not provided
-        if imm_path == 1:
+        if type(imm_path) == int:
             data = pd.read_csv(window_path)
             #check and clean headers
             if data.columns[0] == 'MEASUREMENT_TIME':
@@ -76,7 +82,6 @@ class HMM:
             
         return data
 
-
     """
     Splits data into train and test portions, default 80-20 split
     Training data is first train_size % of the data
@@ -86,7 +91,7 @@ class HMM:
             test_size float percentage of data to be test set
     @return: 2 dfs (train and test dfs)
     """
-    def train_test_split(data,train_size = .8, test_size = .2):
+    def train_test_split(self,data,train_size = .8, test_size = .2):
         len_train_data = train_size * len(data)
         len_test_size = test_size * len(data)
         train_data = data[:int(len_train_data)]
@@ -97,10 +102,10 @@ class HMM:
     #This predictor outputs the top x probable windows and checks the current test data window if it's in the top x.
     #if not then a check mark of incorrect is added 
     #accuracy is outputted at the end
-    def predictor1(train, test): #(self, X)
+    def predictor1(self,train, test):
         window_order = list(train.window.unique())
         #Create transition matrix (store as df for easier indexing) ~ made on train data
-        trans_matrix = pd.DataFrame(transition_matrix(train))
+        trans_matrix = pd.DataFrame(self.transition_matrix(train))
         trans_matrix.columns = window_order
         trans_matrix.index = window_order
         
@@ -116,7 +121,7 @@ class HMM:
                 win_index = window_order.index(win)
                 probs = trans_matrix.loc[win]
                 #setting it as top 5, will be change d depending on accuracy
-                top_prob_wins = probs.sort_values(ascending = False)[:5].index
+                top_prob_wins = probs.sort_values(ascending = False)[:6].index
                 #check if the next window is within the threshold
                 if next_win in top_prob_wins:
                     num_correct +=1
@@ -131,10 +136,10 @@ class HMM:
 
 
     #This predictor ouputs a sequence of next windows. Accuracy is measured AFTER 
-    def predictor2(train,test): #(self, X)
+    def predictor2(self,train,test): #(self, X)
         window_order = list(train.window.unique())
         #Create transition matrix (store as df for easier indexing)
-        trans_matrix = pd.DataFrame(transition_matrix(train))
+        trans_matrix = pd.DataFrame(self.transition_matrix(train))
         trans_matrix.columns = window_order
         trans_matrix.index = window_order
         
@@ -162,11 +167,11 @@ class HMM:
     #if not then a check mark of incorrect is added 
     #accuracy is outputted at the end
 
-    def predictor3(train, test): #(self, X)
+    def predictor3(self,train, test): #(self, X)
 
         window_order = list(train.window.unique())
         #Create transition matrix (store as df for easier indexing)
-        trans_matrix = pd.DataFrame(transition_matrix(train) + emission_matrix(train))
+        trans_matrix = pd.DataFrame(self.transition_matrix(train) + self.emission_matrix(train))
         trans_matrix.columns = window_order
         trans_matrix.index = window_order + ['is_immersive'] #add immersive label for the row-wise index
         
@@ -182,8 +187,8 @@ class HMM:
             if win in window_order:
                 win_index = window_order.index(win)
                 probs = trans_matrix.loc[win]
-                #setting it as top 5, will be changed depending on accuracy
-                top_prob_wins = probs.sort_values(ascending = False)[:5].index
+                #setting it as top x, will be changed depending on accuracy
+                top_prob_wins = probs.sort_values(ascending = False)[:7].index
                 #check if the next window is within the threshold AND if they're immersive
                 #the immersive part is redundant since its just binary values that are always related to the its respectable window
                 if (next_win in top_prob_wins) and (next_win_imm == trans_matrix.loc['is_immersive', next_win]):
@@ -193,7 +198,7 @@ class HMM:
             #ex. if train data has more 0 is_immersive than 1, then we will make 0 the "is correct" condition
             # if so, then its a correct prediction
             else:
-                top_windows = train.window.value_counts(ascending = False)[:5].index
+                top_windows = train.window.value_counts(ascending = False)[:7].index
                 immersive_value = train.is_immersive.value_counts().idxmax()
                 if (next_win in top_windows) and (next_win_imm == immersive_value):
                     num_correct +=1
